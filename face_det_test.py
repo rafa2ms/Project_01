6606f7a30163253de8a021406e3bad63148baa58
rafa2ms <rafael.monteiro2ms@gmail.com>
2023-09-19 17:16:38 +0200

    Revert "Upgrade of 'face_det_test.py' because was made in the wrong branch"
    This reverts commit 13518a98e2bb0eb30c7e66d3002d94ad3020b378.

--- face_det_test.py
+++ face_det_test.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -9,50 +8,22 @@ cap = cv2.VideoCapture(0)
 # To use a video file as input 
 # cap = cv2.VideoCapture('filename.mp4')
 
-#Aligment options
-#	(l)eft
-#	(c)enter
-#	(r)ight
-
-color = [(230, 25, 75),
-	(60, 180, 75),
-	(255, 225, 25),
-	(0, 130, 200),
-	(245, 130, 48),
-	(145, 30, 180),
-	(70, 240, 240),
-	(240, 50, 230),
-	(210, 245, 60),
-	(128, 128, 128)]
-
-c = np.random.randint(0, 256,size=(10,3))
-
 while True:
-
-	# Read the frame
-	_, img = cap.read()
-	# Convert to grayscale
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
- 	# Detect the faces
-	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-	# Draw the rectangle around each face
-	n_face = 0
-
-	for (x, y, w, h) in faces:
-		##index = np.random.randint(0, len(color))
-		tup = tuple(c[n_face])
-		rgb = (int(tup[0]), int(tup[1]) ,int(tup[2]))
-		rgb = color[n_face]
-		cv2.rectangle(img, (x, y), (x+w, y+h), rgb, 2)
-		# draw_label(img, "Face "+str(n_face), (x, y+h), 'l', c)
-		# Display
-		cv2.imshow('img', img)
-		n_face = n_face + 1
-
-	# Stop if escape key is pressed
-	k = cv2.waitKey(30) & 0xff
-	if k==27:
-		break
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
 # Release the VideoCapture object
 cap.release()
 
