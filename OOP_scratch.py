d40dc9833b1b00abb032656dc457cd817274df7e
rafa2ms <rafael.monteiro2ms@gmail.com>
2023-09-21 16:03:57 +0200

    Restruct the script to test its behavior as a library

--- OOP_scratch.py
+++ OOP_scratch.py
@@ -41,10 +41,10 @@ class Label:
         elif self.align == 'C':
             pos_x = pos_x - round(txt_size[0][0]/ 2)
 
-        '''
-        elif align == 'R':
-            pos_x = pos_x - round(txt_size[0][0]/ 2)
-        '''
+        elif self.align == 'R':
+            pos_y = pos_y + txt_size[0][1] + self.margin
+            pos_x = pos_x - txt_size[0][0]
+        
         
         end_x = pos_x + txt_size[0][0] + self.margin
         end_y = pos_y - txt_size[0][1] - self.margin
@@ -52,10 +52,14 @@ class Label:
         cv2.rectangle(img, (pos_x - self.margin, pos_y + self.margin), (end_x, end_y), self.bg_color, self.thickness)
         cv2.putText(img, self.text, (pos_x, pos_y), self.font_face, self.scale, self.txt_color, 1, cv2.LINE_AA)
 
-cam_port = 0
-cam = VideoCapture(cam_port)
 
-while True:
+# Better set cam_port as a parameter, once it may change from
+# one pc to another. However, let's suppose that the port #zero
+# is the most common and set it as default
+
+def time_snapshot(cam_port = 0):
+    cam = VideoCapture(cam_port)
+
     result, image = cam.read()
     image = cv2.flip(image,1)
 
@@ -65,28 +69,25 @@ while True:
         w = round(width/2)
         h = height-20
         
-        time_label = Label(pos = (w,h), scale=0.8, align='C')#bg_color = (200,213,48)
+        time_label = Label(pos = (w,h), scale=0.8, align='C')
         time_label.set_time()
         time_label.draw_label(image)
-        
-        label_1 = Label(text = "OOP provides", txt_color=(0,0,255), scale=0.6)
-        label_1.draw_label(image)
-
-        label_2 = Label(text = "easier data", pos = (20, 45), margin=5, font_face=cv2.FONT_HERSHEY_TRIPLEX,
-                        txt_color=(255,255,255), bg_color=(0,0,255), scale=0.8)
-        label_2.draw_label(image)
-        
-        label_3 = Label(text = "manipulation", pos = (60, 85), margin=10, font_face=cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
-                        txt_color=(0, 231, 255), bg_color=(73, 158, 0), scale=1)
-        label_3.draw_label(image)
 
         imshow("View",image)
-
-        #imwrite("View.png",image)
+        imwrite("Snapshot.png",image)
 
     else:
         print("No image detected. Please, try again.")
 
+
+# As long as the code is not part of a library yet,
+# this infinity loop routine will keep the window open 
+# until the escape key be pressed
+
+# =============================================
+time_snapshot()
+while True:
     k = cv2.waitKey(30) & 0xff
     if k ==27: break
-    if k ==32: imwrite("Snapshot.png",image)
+# =============================================
+
